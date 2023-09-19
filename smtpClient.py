4f09ca7fbd4107cb01ca7af794ae7674906ffbb3
aurieStudent <144954692+aurieStudent@users.noreply.github.com>
2023-09-19 13:21:22 -0400

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -29,22 +29,22 @@ def smtp_client(port=1025, mailserver='127.0.0.1'): #lord I didn't choose a mail
     # Fill in start
     mailFrom="MAIL FROM: <auriecat@gmail.com>\r\n"
     clientSocket.send(mailFrom.encode())
-    recv2=clientSocket.recv(1024).decode()
+    recv2=clientSocket.recv(1024)
 
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
-    rcptTo=" RCPT TO: <ac10984@nyu.edu>\r\n"
+    rcptTo="RCPT TO: <ac10984@nyu.edu>\r\n"
     clientSocket.send(rcptTo.encode())
-    recv3=clientSocket.recv(1024).decode() #test
+    recv3=clientSocket.recv(1024) #test
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
     dataCo = "DATA\r\n"
     clientSocket.send(dataCo.encode())
-    recv4=clientSocket.recv(1024).decode()
+    recv4 = clientSocket.recv(1024)
 
 
     # Fill in end
@@ -54,13 +54,13 @@ def smtp_client(port=1025, mailserver='127.0.0.1'): #lord I didn't choose a mail
     #clientSocket.send(rcptTo) #test
     msg = dataCo + mailFrom + rcptTo + msg
     clientSocket.send(msg.encode())
-    recv5=clientSocket.recv(1024).decode()
+    recv5=clientSocket.recv(1024)
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response. #end msg has the period
     # Fill in start
     clientSocket.send(endmsg.encode())
-    recv6=clientSocket.recv(1024).decode()
+    recv6=clientSocket.recv(1024)
     # Fill in end
 
     # Send QUIT command and handle server response.
